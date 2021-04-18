a615ad877f705ce6c9ff6182342742ecf01b62a9
AliHaidar110 <64657659+AliHaidar110@users.noreply.github.com>
2021-04-18 03:03:59 +0300

    initial commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,33 +1,42 @@
+import { useState } from "react";
 import CartItem from "./CartItem";
+import ConfirmationPopUp from "./ConfirmationPopUp";
 
 import {
   EmptyCart,
-   Container,
-    TotalPrice,
-     CartItems, 
-      ClearCart,
-       BottomRow } from "./styled";
+  Container,
+  TotalPrice,
+  CartItems,
+  ClearCart,
+  BottomRow
+} from "./styled";
 
 const Cart = ({ cart, removeFromCart, removeAll }) => {
-   if(!cart.length) {
-     return <EmptyCart>your cart is currently empty</EmptyCart>
-   }
+  const [showPop, setShowPop] = useState(false);
+
+  if (!cart.length) {
+    return <EmptyCart>your cart is currently empty</EmptyCart>;
+  }
   const cartItems = cart.map((item) => (
     <CartItem key={item.id} product={item} removeFromCart={removeFromCart} />
   ));
   return (
     <Container>
-     <CartItems>{cartItems}</CartItems>
-     <BottomRow>
-     <TotalPrice>
-      Total Price: $
-      {cart.reduce((total, item) => total + item.price * item.count, 0)}
-      </TotalPrice>
-      <ClearCart onClick={removeAll}>Delete All</ClearCart>
-      
-      
+      <CartItems>{cartItems}</CartItems>
+      <BottomRow>
+        <TotalPrice>
+          Total Price: $
+          {cart.reduce((total, item) => total + item.price * item.count, 0)}
+        </TotalPrice>
+        <ClearCart disabled={showPop} onClick={() => setShowPop(true)}>
+          Delete All
+        </ClearCart>
+        <ConfirmationPopUp
+          show={showPop}
+          onCancel={() => setShowPop(false)}
+          onConfirm={() => removeAll()}
+        />
       </BottomRow>
-      
     </Container>
   );
 };
